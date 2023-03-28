b0b563b56e09779a8cb3a213bdd33e575fcb43be
Obinna Ukwa <obikelv@gmail.com>
2023-03-28 11:21:20 -0700

    Updating print functions

---
+++ funcs_printf.c
@@ -0,0 +1,80 @@
+#include "main.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * chr_p - print character
+ * @c: char argument
+ * Return: length of string
+ */
+
+int chr_p(char c)
+{
+_putchar(c);
+return (1);
+}
+
+
+/**
+ * str_p - print string
+ * @s: pointer to char argument
+ * Return: length of string
+ */
+
+int str_p(char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+_putchar(s[i++]);
+return (strlen(s) - 1);
+}
+
+/**
+ * int_p - print character
+ * @i: int argument
+ * Return: length of string
+ */
+
+int int_p(int i)
+{
+int p = 0;
+char *s = tostring(i);
+while (s[p] != '\0')
+_putchar(s[p++]);
+return (strlen(s) - 1);
+}
+
+/**
+ * deci_p - print character
+ * @i: char argument
+ * Return: length of string
+ */
+
+int deci_p(int i)
+{
+int j = 0;
+char *s = tostring(i);
+
+while (s[j] != '\0')
+_putchar(s[j++]);
+return (strlen(s) - 1);
+}
+
+/**
+ * uns_int_p - print character
+ * @i: unsigned integer argument
+ * Return: length of string
+ */
+
+int uns_int_p(unsigned int i)
+{
+int j = 0;
+char *s = tostring(i);
+
+while (s[j] != '\0')
+_putchar(s[j++]);
+return (strlen(s) - 1);
+}
+
---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN
+#define MAIN
+#include <stdarg.h>
+
+char *tostring(int num);
+char *tohex_oct(unsigned int num, char c, unsigned int base);
+int chr_p(char c);
+int str_p(char *s);
+int int_p(int i);
+int deci_p(int i);
+int _putchar(char c);
+int uns_int_p(unsigned int i);
+int uns_hex_p(unsigned int i, char c);
+int uns_oct_p(unsigned int deci_n);
+void args_int_p(int v, char c, int *count);
+void args_int_p(int v, char c, int *count);
+void args_uint_p(unsigned int v, char c, int *count);
+void args_p(int *count);
+#endif
